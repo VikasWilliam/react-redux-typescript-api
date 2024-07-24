d4a37335b35269e3e8d3a4652a2b1f75be581fa2
unknown <vikasdenobili@outlook.com>
2024-07-24 13:13:17 +0530

    initial commit Project setup

---
+++ reduxtsapp/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return <div>
+    <h2 style={{backgroundColor:"orange"}}>Simple Redux app with ts</h2>
+    <button style={{backgroundColor:"lightblue"}}>Click me</button>
+    </div>;
+}
+
+export default App;
